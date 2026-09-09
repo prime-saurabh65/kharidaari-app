f46cb4f60c9188c5251cb13199ffdd06729f71cf
Saurabh Shivam <shivamsaurabh4uk@gmail.com>
2026-09-09 14:56:32 +0530

    Fixed App.jsx bug problem for Navbar.jsx import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/Home/Home'
 import Products from './pages/Products/Products'
 import ProductDetails from './pages/ProductDetails/ProductDetails'
 import Footer from './components/Footer/Footer'
-import Navbar from './components/NavBar/NavBar'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
