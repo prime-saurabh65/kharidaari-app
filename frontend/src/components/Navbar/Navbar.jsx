ce940382b31e2a43b34adc0c50d5c68014fb1f25
Saurabh Shivam <shivamsaurabh4uk@gmail.com>
2026-09-09 14:51:42 +0530

    added Navbar html code structures

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <div className='flex justify-between'>
+            <h1>kharidaari</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Service</li>
+                <li>Helpers</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <h1 className='text-3xl text-black-800'>This is the Home page</h1>
+  )
+}
+
+export default Home
