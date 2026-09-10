5cfa45fb0221c9fa41d93fc7c6d5c566d8d3619e
Saurabh Shivam <shivamsaurabh4uk@gmail.com>
2026-09-10 23:59:02 +0530

    designed footer part

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Products from './pages/Products/Products'
-import ProductDetails from './pages/ProductDetails/ProductDetails'
-import Footer from './components/Footer/Footer'
-import Navbar from './components/Navbar/Navbar'
-import NotFound from './pages/NotFound/NotFound'
+import { useState } from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import ProductDetails from "./pages/ProductDetails/ProductDetails";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Navbar />
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
 
-      <Footer />
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-gray-700 h-60'>
+      <h1 className='text-white bg-gray-600 h-2/12 flex items-center justify-center'>
+        Back to top
+      </h1>
+
+      <div className='grid grid-cols-12 gap-4'>
+        <div>
+          <h1 className='text-xl'>Get to Know Us</h1>
+        </div>
+        <div></div>
+        <div></div>
+
+      </div>
+
+    </div>
   )
 }
 
